4443c942418baae12e6487ed5df50b46a33d8c5d
SePt1k0404 <99293859+SePt1k0404@users.noreply.github.com>
2023-11-26 16:17:41 +0200

    fix bug with case when authorization field not specified

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -5,28 +5,32 @@ import User from "../models/User.js";
 const { JWT_SECRET } = process.env;
 
 const authenticate = async (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization) {
-    const error = new Error(`Authorization header not found!`);
-    error.status = 401;
-    throw error;
-  }
-  const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
-    const error = new Error(`Not authorized`);
-    error.status = 401;
-    throw error;
-  }
   try {
-    const { id } = jwt.verify(token, JWT_SECRET);
-    const user = await User.findById(id);
-    if (!user || !user.token || user.token !== token) {
+    const { authorization } = req.headers;
+    if (authorization === undefined) {
+      const error = new Error(`Authorization header not found!`);
+      error.status = 401;
+      throw error;
+    }
+    const [bearer, token] = authorization.split(" ");
+    if (bearer !== "Bearer") {
       const error = new Error(`Not authorized`);
       error.status = 401;
       throw error;
     }
-    req.user = user;
-    next();
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+      const user = await User.findById(id);
+      if (!user || !user.token || user.token !== token) {
+        const error = new Error(`Not authorized`);
+        error.status = 401;
+        throw error;
+      }
+      req.user = user;
+      next();
+    } catch (error) {
+      next(error);
+    }
   } catch (error) {
     next(error);
   }
